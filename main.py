8c0c9de3e6a891c45c99ae651f2f40b6335219f4
Tega Okene <okenetega@gmail.com>
2025-02-21 19:53:02 -0600

    Move models code to dedicated folder.

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import logging
-import os
-import re
 
-from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
-from openai import OpenAI
+
+from models.models import fact_checker
 
 app = FastAPI()
-
-load_dotenv()
 
 logging.basicConfig(
     filename="app.log",
@@ -18,56 +14,14 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_KEY")
-
-NOT_APPLICABLE_CODE = "CHECKFACT"
-API_PROVIDER_URL = "https://openrouter.ai/api/v1"
-MODEL = "meta-llama/llama-3.3-70b-instruct:free"
-SYSTEM_PROMPT = (
-    "You are a fact checking assistant. Use reliable, evidence-based sources. "
-    "You give only the percentage on how true a statement is. "
-    "If there is a statement such as 'I am pregnant' where it is "
-    f"impossible to know if the user is pregnant, add '{NOT_APPLICABLE_CODE}' to the response."
-)
-
 
 @app.post("/fact")
 def fact_check(payload: dict):
     text = payload.get("text", "")
 
     if not text:
-        raise HTTPException(status_code=404, detail="Text not found")
+        raise HTTPException(status_code=400, detail="Text not provided")
 
-    response = model(text)
+    response = fact_checker(text)
     logger.info(response)
     return {"message": response}
-
-
-def model(content):
-    logger.info(f'Requested text: "{content}"')
-    client = OpenAI(base_url=API_PROVIDER_URL, api_key=OPENROUTER_API_KEY)
-    completion = client.chat.completions.create(
-        model=MODEL,
-        messages=[
-            {
-                "role": "system",
-                "content": SYSTEM_PROMPT,
-            },
-            {"role": "user", "content": content},
-        ],
-    )
-    message = completion.choices[0].message
-    logger.info(message)
-    result = extract_result(message.content)
-    return result
-
-
-def extract_result(input_str):
-    if NOT_APPLICABLE_CODE in input_str:
-        return NOT_APPLICABLE_CODE
-
-    match = re.match(r"(\d+%)(.*)", input_str)
-    if match:
-        percentage = match.group(1).strip().rstrip("%")
-        return percentage
-    return None
---
+++ models/models.py
@@ -0,0 +1,51 @@
+import logging
+import os
+import re
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_KEY")
+NOT_APPLICABLE_CODE = "NOT_APPLICABLE"
+API_PROVIDER_URL = "https://openrouter.ai/api/v1"
+MODEL = "meta-llama/llama-3.3-70b-instruct:free"
+SYSTEM_PROMPT = (
+    "You are a fact checking assistant. Use reliable, evidence-based sources. "
+    "You give only the percentage on how true a statement is. "
+    "If there is a statement such as 'I am pregnant' where it is "
+    f"impossible to know if the user is pregnant, add '{NOT_APPLICABLE_CODE}' to the response."
+)
+
+
+def fact_checker(content):
+    logger.info(f'Requested text: "{content}"')
+    client = OpenAI(base_url=API_PROVIDER_URL, api_key=OPENROUTER_API_KEY)
+    completion = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT,
+            },
+            {"role": "user", "content": content},
+        ],
+    )
+    message = completion.choices[0].message
+    logger.info(message)
+    result = extract_result(message.content)
+    return result
+
+
+def extract_result(input_str):
+    if NOT_APPLICABLE_CODE in input_str:
+        return NOT_APPLICABLE_CODE
+
+    match = re.match(r"(\d+%)(.*)", input_str)
+    if match:
+        percentage = match.group(1).strip().rstrip("%")
+        return percentage
+    return None
